5feed2014dcfd0227d1a5e2f86bc024c6abe1cfe
Andy Shen <andyshenproper@gmail.com>
2022-06-03 10:38:39 -0700

    change scroll behaviour

--- js/custom.js
+++ js/custom.js
@@ -6,3 +6,30 @@ function getYear() {
 }
 
 getYear();
+
+window.onscroll = function() {navbarHighlight()};
+
+const navitems = document.querySelectorAll("nav ul li");
+console.log(navitems)
+const sections = document.querySelectorAll("section");
+
+function navbarHighlight() {
+    let current = "";
+
+    sections.forEach( section => {
+        sectionTop = section.offsetTop - 80;
+        sectionHeight = section.clientHeight + 80;
+        if (scrollY >= sectionTop && scrollY <= sectionTop + sectionHeight) {
+            current = section.getAttribute('id');
+            console.log(current)
+        } 
+    })
+
+    navitems.forEach( navitem => {
+        navitem.classList.remove("active");
+        if (navitem.classList.contains(current)) {
+            navitem.classList.add("active");
+            console.log(navitem.classList)
+        }
+    })
+}
